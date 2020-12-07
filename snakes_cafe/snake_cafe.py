1ef0c7c894a8364bf445a2077a548f404631e6c1
Ashley Casimir <ashcas@me.com>
2020-12-06 20:19:51 -0800

    changed all items to dictionary, created a function for handling orders

--- snakes_cafe/snake_cafe.py
+++ snakes_cafe/snake_cafe.py
@@ -1,10 +1,12 @@
 
 #Variables for menu
+menu = {
+  'appetizers': ['Wings', 'Cookies', 'Spring Rolls'],
+  'entrees': ['Salmon', 'Steak', 'Meat Tornado', 'A Literal Garden'],
+  'desserts': ['Ice Cream', 'Cake', 'Pie'],
+  'drinks': ['Coffee', 'Tea', 'Unicorn Tears']
+}
 
-appetizers = ['Wings', 'Cookies', 'Spring Rolls']
-entrees = ['Salmon', 'Steak', 'Meat Tornado', 'A Literal Garden']
-desserts = ['Ice Cream', 'Cake', 'Pie']
-drinks = ['Coffee', 'Tea', 'Unicorn Tears']
 
 #Variables for the menu
 header = '*' * 37
@@ -20,11 +22,6 @@ welcome = """
 menu_details = "-" * 10
 order_question = '\n**  What would you like to order?  **\n'
 
-
-#Function for keeping track of order
-
-  
-
 #Function for menu header and footer
 def create_menu_header(text):
   print(header)
@@ -38,16 +35,35 @@ def create_menu_sections(title, menu_items_list):
   for item in menu_items_list:
     print(f'{item}\n')
 
+#Function to track orders
+def order_tracker():
+  order = {}
+  user_quit = False
+  
+  while not user_quit:
+    user_input = input('\n> ')
 
+    if user_input.lower() == 'quit':
+      return
+    else:
+      #Kept getting KeyError and Remebered from doing the prework on Sololearn about try and except!
+      try:
+        order[user_input] += 1
+        print(f'\n** {order[user_input]} orders of {user_input} have been added to your meal **')
+      except:
+        order[user_input] = 1
+        print(f'\n** {order[user_input]} order of {user_input} have been added to your meal **')
 
 create_menu_header(welcome)
 
-create_menu_sections('Appeitizers', appetizers)
-create_menu_sections('Entrees', entrees)
-create_menu_sections('Desserts', desserts)
-create_menu_sections('Drinks', drinks)
+create_menu_sections('Appeitizers', menu['appetizers'])
+create_menu_sections('Entrees', menu['entrees'])
+create_menu_sections('Desserts', menu['desserts'])
+create_menu_sections('Drinks', menu['drinks'])
 
 create_menu_header(order_question)
-order = {}
-user_input = input("\n>")
-print(user_input)
+
+order_tracker()
+
+
+  
