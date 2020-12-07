ff7da0ef12888bfaee2af82d8f432269e8792a4d
Ashley Casimir <ashcas@me.com>
2020-12-06 18:42:02 -0800

    welcome header created

---
+++ snakes_cafe/snake_cafe.py
@@ -0,0 +1,18 @@
+appetizers = ['Wings', 'Cookies', 'Spring Rolls']
+entrees = ['Salmon', 'Steak', 'Meat Tornado', 'A Literal Garden']
+desserts = ['Ice Cream', 'Cake', 'Pie']
+drinks = ['Coffee', 'Tea', 'Unicorn Tears']
+headers = '*' * 37
+welcome = """
+**   Welcome to the Snakes Cafe!   **
+
+**    Please see our menu below.   **
+
+**                                 **
+
+** To quit at anytime, type 'quit' **
+"""
+
+print(headers)
+print(welcome)
+print(headers)
